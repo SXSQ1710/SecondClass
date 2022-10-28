c975d42677099298f23781f0d45e165b300e460a
SXSQ <1710987943@qq.com>
2022-10-28 11:15:26 +0800

    1028 数据库更新

--- sc-api/secondclass.sql
+++ sc-api/secondclass.sql
@@ -11,29 +11,29 @@
  Target Server Version : 80026
  File Encoding         : 65001
 
- Date: 27/10/2022 17:24:34
+ Date: 28/10/2022 11:14:52
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for t_shichang
+-- Table structure for t_shi_chang
 -- ----------------------------
-DROP TABLE IF EXISTS `t_shichang`;
-CREATE TABLE `t_shichang`  (
+DROP TABLE IF EXISTS `t_shi_chang`;
+CREATE TABLE `t_shi_chang`  (
   `sid` bigint NOT NULL AUTO_INCREMENT COMMENT '时长id',
   `shichang_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '时长类型',
   PRIMARY KEY (`sid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of t_shichang
+-- Records of t_shi_chang
 -- ----------------------------
-INSERT INTO `t_shichang` VALUES (1, '文体艺术活动');
-INSERT INTO `t_shichang` VALUES (2, '实践志愿活动');
-INSERT INTO `t_shichang` VALUES (3, '双创实训活动');
-INSERT INTO `t_shichang` VALUES (4, '校园建设活动');
-INSERT INTO `t_shichang` VALUES (5, '理想信念活动');
+INSERT INTO `t_shi_chang` VALUES (1, '文体艺术活动');
+INSERT INTO `t_shi_chang` VALUES (2, '实践志愿活动');
+INSERT INTO `t_shi_chang` VALUES (3, '双创实训活动');
+INSERT INTO `t_shi_chang` VALUES (4, '校园建设活动');
+INSERT INTO `t_shi_chang` VALUES (5, '理想信念活动');
 
 SET FOREIGN_KEY_CHECKS = 1;
