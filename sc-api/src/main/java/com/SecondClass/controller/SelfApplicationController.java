88e67905cc2a35df0b65ad668eb9dddddc089e18
zlj0625 <2234896756@qq.com>
2022-11-03 18:39:42 +0800

    新增提交审核申报表和查看申报结果

--- sc-api/src/main/java/com/SecondClass/controller/SelfApplicationController.java
+++ sc-api/src/main/java/com/SecondClass/controller/SelfApplicationController.java
@@ -3,6 +3,7 @@ package com.SecondClass.controller;
 import com.SecondClass.entity.Response;
 import com.SecondClass.entity.SelfApplication;
 import com.SecondClass.server.ISelfApplicationServer;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,6 +25,20 @@ public class SelfApplicationController {
     @PostMapping("/addSelfApplicationAttachment")
     public Response addSelfApplicationAttachment(@RequestParam MultipartFile attachment){
         return selfApplicationServer.addSelfApplicationAttachment(attachment);
+    }
+
+    @GetMapping("/auditSelfApplication/{selfAppId}/{statue}")
+    public Response auditSelfApplication(@PathVariable("selfAppId") Integer selfAppId,
+                                         @PathVariable("statue") Integer statue) {
+        return selfApplicationServer.auditSelfApplication(selfAppId, statue);
+    }
+
+    @GetMapping("/getMySelfApplication/{uid}/{pageNo}/{pageSize}")
+    public Response getMySelfApplication(@PathVariable("uid") Integer uid,
+                                         @PathVariable("pageNo") Integer pageNo,
+                                         @PathVariable("pageSize") Integer pageSize) {
+        Page<SelfApplication> page = new Page<>(pageNo,pageSize);
+        return selfApplicationServer.getMySelfApplication(uid,page);
     }
 
 }
--- sc-api/src/main/java/com/SecondClass/server/ISelfApplicationServer.java
+++ sc-api/src/main/java/com/SecondClass/server/ISelfApplicationServer.java
@@ -2,6 +2,7 @@ package com.SecondClass.server;
 
 import com.SecondClass.entity.Response;
 import com.SecondClass.entity.SelfApplication;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ISelfApplicationServer{
@@ -19,4 +20,20 @@ public interface ISelfApplicationServer{
      * @return Response
      */
     Response addSelfApplicationAttachment(MultipartFile attachment);
+
+    /**
+     * 审核申请表
+     * @param selfAppId 自主申报表Id
+     * @param statue 审核状态 2：审核中 1：通过 0：拒绝
+     * @return Response
+     */
+    Response auditSelfApplication(Integer selfAppId,Integer statue);
+
+    /**
+     * 查看申报结果（即浏览申报记录）
+     * @param uid 用户id
+     * @param page 分页
+     * @return Response
+     */
+    Response getMySelfApplication(Integer uid, Page<SelfApplication> page);
 }
