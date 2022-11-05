dc79e6752a75228a11d189931cd823f69e37d9a6
hailyeJiang <302685516@qq.com>
2022-11-05 19:21:55 +0800

    11.5 controller

---
+++ sc-api/src/main/java/com/SecondClass/server/ManageServerImpl.java
@@ -0,0 +1,29 @@
+package com.SecondClass.server;
+
+import com.SecondClass.entity.Oganization;
+import com.SecondClass.entity.R_entity.R_SignIn;
+import com.SecondClass.entity.Response;
+import com.SecondClass.entity.User;
+
+/**
+ * @ClassName: ManageServerImpl
+ * @Description //TODO
+ * @Author jiang
+ * @Date 2022/11/5 19:03
+ **/
+public class ManageServerImpl {
+
+    public Response login(R_SignIn request) {
+        return null;
+    }
+
+    public Response createOrg(Oganization request) {
+        return null;
+
+    }
+
+    public Response addAccount(User request) {
+        return null;
+
+    }
+}
