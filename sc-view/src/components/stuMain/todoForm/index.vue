931e66461d3db4f767731bf3b0dbefc84c1942d7
hailyeJiang <302685516@qq.com>
2022-11-16 16:09:51 +0800

    11.16 自主申报页面

--- sc-view/src/components/stuMain/todoForm/index.vue
+++ sc-view/src/components/stuMain/todoForm/index.vue
@@ -1,9 +1,91 @@
 <template>
-    <h1>你好，我是todoForm</h1>
-    </template>
+    <h1>时长自主申报</h1>
+    <div class="demo-collapse">
+        <el-collapse style="font-weight: 600;" v-model="activeNames" @change="handleChange">
+            <el-collapse-item title="校外实践时长申报" name="1">
+                <div style="margin-left:10px">
+                    <el-button type="primary" :icon="Edit" round>申报时长</el-button>
+                </div>
+                <div>
+                    <el-table :data="tableData" style="min-width: 100%;" height="250">
+                        <el-table-column prop="uid" label="学号" width="150" />
+                        <el-table-column prop="selfAppType" label="时长类型" width="150" />
+                        <el-table-column prop="selfAppShiNum" label="时长数" width="150" />
+                        <el-table-column prop="selfAppDescription" label="时长描述" width="200" />
+                        <el-table-column prop="selfAppAttachment" label="查看附件" width="120" />
+                        <el-table-column fixed="right" prop="selfAppStatu" label="申报状态" width="120"></el-table-column>
+                    </el-table>
+                </div>
+            </el-collapse-item>
+            <el-collapse-item title="其他类型时长申报" name="2">
+                <div>
+                    <div>
+                        <el-table :data="tableData" style="min-width: 100%;" height="250">
+                        <el-table-column prop="uid" label="学号" width="150" />
+                        <el-table-column prop="selfAppType" label="时长类型" width="150" />
+                        <el-table-column prop="selfAppShiNum" label="时长数" width="150" />
+                        <el-table-column prop="selfAppDescription" label="时长描述" width="200" />
+                        <el-table-column prop="selfAppAttachment" label="查看附件" width="120" />
+                        <el-table-column fixed="right" prop="selfAppStatu" label="申报状态" width="120"></el-table-column>
+                    </el-table>
+                </div>
+                </div>
+            </el-collapse-item>
+        </el-collapse>
+    </div>
+</template>
     
-    <script>
-    export default{
-        name:"todoForm"
-    }
-    </script>
+<script>
+export default {
+    name: "todoForm"
+}
+</script>
+<script setup>
+import { ref } from 'vue'
+import {Edit} from '@element-plus/icons-vue'
+import {getMySelfApplication} from '@/server/api/shiChang'
+
+const activeNames = ref(['1'])
+const handleChange = (val) => {
+    console.log(val)
+}
+const handleClick = () => {
+    console.log('click')
+}
+
+const Data = () =>{
+    getMySelfApplication(uid,pageNo,pageSize).then(function (response) {
+    return response;
+  });
+}
+
+// const Data = [
+//         uid,
+//         selfAppType,
+//         selfAppShiNum,
+//         selfAppDescription,
+//         selfAppAttachment,
+//         selfAppStatu
+// ]
+const tableData = [
+    {
+        uid:'3220001111',
+        selfAppType:'志愿时长',
+        selfAppShiNum:'10',
+        selfAppDescription:'献血',
+        selfAppAttachment:'附件',
+        selfAppStatu:'待审核'
+    },
+    {
+        uid:'3220001111',
+        selfAppType:'双创时长',
+        selfAppShiNum:'40',
+        selfAppDescription:'实习',
+        selfAppAttachment:'附件',
+        selfAppStatu:'已通过'
+    },
+]
+</script>
+<style>
+
+</style>
---
+++ sc-view/src/server/api/shiChang.js
@@ -0,0 +1,6 @@
+import axios from '@/axios'
+
+export function getMySelfApplication(uid,pageNo,pageSize){
+    return axios.get("/api/selfApplication/getMySelfApplication/{uid}/{pageNo}/{pageSize}")
+
+}
