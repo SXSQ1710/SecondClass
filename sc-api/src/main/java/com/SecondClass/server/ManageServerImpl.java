f5b7143e258ef648ae18c4c163d6adc9e97e4caf
SXSQ <1710987943@qq.com>
2022-12-13 18:55:26 +0800

    1213 组织管理完善，数据库更新

--- sc-api/src/main/java/com/SecondClass/server/ManageServerImpl.java
+++ sc-api/src/main/java/com/SecondClass/server/ManageServerImpl.java
@@ -529,6 +529,7 @@ public class ManageServerImpl extends ServiceImpl<UserMapper,User> implements IM
      * @Param MultipartFile
      * @return com.SecondClass.entity.Response
      **/
+
     @Transactional
     public Response addAccountByBatch(MultipartFile file) {
         try {
